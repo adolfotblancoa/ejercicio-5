377fb8bfb864971d75be4b71fe947544c7b4f3af
adolfotblancoa <adolfotomas.blancoaraujo@usp.ceu.es>
2023-03-14 10:22:43 +0100

    ejer 14.8

--- aplicacion/Principal.java
+++ aplicacion/Principal.java
@@ -1,19 +1,45 @@
 package aplicacion;
+import java.util.Arrays;
+import java.util.List;
 public class Principal {
 
     public static void main(String[] args) {
         
-        //ejer1
-        System.out.println("Ejercicio 1: Dada una lista de números, obtiene otra lista con sus cuadrados.");
-        int[] lista = {1, 2, 3, 4, 5};
+        System.out.println("Ejercicio 12: Explique que es una expresión lambda: \n R: Es una función anónima en programación que se define sin un nombre y se puede pasar como argumento a otras funciones.");
+        
+        System.out.println("Ejercicio 14: ");
+        //ejer 14.1
+        System.out.println("1: Dada una lista de números, obtiene otra lista con sus cuadrados.");
+        List<Integer> lista = Arrays.asList(1, 2, 3, 4, 5);
         dominio.Matematicas.listaCuadrados(lista);
-
-        //ejer2
-        System.out.println("Ejercicio 2: Dada una lista de números, obtiene su suma.");
-        int[] lista2 = {1, 2, 3, 4, 5};
+        //ejer 14.2
+        System.out.println("2: Dada una lista de números, obtiene su suma.");
+        List<Integer> lista2 = Arrays.asList(1, 2, 3, 4, 5);
         dominio.Matematicas.sumaLista(lista2);
-
-
+        //ejer 14.3
+        System.out.println("3: Dada una lista de números, obtiene la suma de los numeros pares.");
+        List<Integer> lista3 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        dominio.Matematicas.sumaPares(lista3);
+        //ejer 14.4
+        System.out.println("4: Dada una lista de números, obtiene otra lista con los números pares.");
+        List<Integer> lista4 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        dominio.Matematicas.paresLista(lista4);
+        //ejer 14.5
+        System.out.println("5: La media aritmética de los elementos de una lista dada.");
+        List<Integer> lista5 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        dominio.Matematicas.mediaAritmetica(lista5);
+        //ejer 14.6
+        System.out.println("6. La desviación típica de los elementos de una lista dada.");
+        List<Integer> lista6 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        dominio.Matematicas.desviacionTipica(lista6);
+        //ejer 14.7
+        System.out.println("7. El menor de una lista de números.");
+        List<Integer> lista7 = Arrays.asList(1, 2, 3, 4, 5);
+        dominio.Matematicas.menorLista(lista7);
+        //ejer 14.8
+        System.out.println("8. A partir de una lista de String, obtiene otra lista con sus elementos transformados a minúscula. ");
+        List<String> lista8 = Arrays.asList("ADOlfo", "TOMAs", "blANco", "ARauJO");
+        dominio.Matematicas.aMinusculas(lista8);
     }
     
 }
--- dominio/Matematicas.java
+++ dominio/Matematicas.java
@@ -5,20 +5,67 @@ import java.util.stream.Collectors;
 
 public class Matematicas{
 
-    public static void listaCuadrados(int[] lista){
-        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
-        List<Integer> cuadrados = numeros.stream()
-                                          .map(x -> x * x)
-                                          .collect(Collectors.toList());
-        System.out.println(cuadrados);
+    public static void listaCuadrados(List<Integer> numeros){
         
+        List<Integer> cuadrados = numeros.stream()
+                                .map(x -> x * x)
+                                .collect(Collectors.toList());
+        System.out.println(cuadrados);
     }
 
-    public static void sumaLista(int[] lista){
-        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+    public static void sumaLista(List<Integer> numeros){
         int suma = numeros.stream()
                   .reduce(0, (a, b) -> a + b);
         System.out.println(suma);
+    }
 
+    public static void sumaPares(List<Integer> numeros){
+        int suma = numeros.stream()
+                  .filter(x -> x % 2 == 0)
+                  .reduce(0, (a, b) -> a + b);
+        System.out.println(suma);
+    }
+
+    public static void paresLista(List<Integer> numeros){
+        List<Integer> pares = numeros.stream()
+                    .filter(x -> x % 2 == 0)
+                    .collect(Collectors.toList());
+        System.out.println(pares);
+
+    }
+
+    public static void mediaAritmetica(List<Integer> numeros){
+        double media = numeros.stream()
+                    .mapToInt(x -> x)
+                    .average()
+                    .getAsDouble();
+        System.out.println(media);
+    }
+
+    public static void desviacionTipica(List<Integer> numeros){
+        double media = numeros.stream()
+                    .mapToInt(x -> x)
+                    .average()
+                    .getAsDouble();
+        double desviacion = numeros.stream()
+                    .mapToDouble(x -> Math.pow(x - media, 2))
+                    .average()
+                    .getAsDouble();
+        System.out.println(Math.sqrt(desviacion));
+    }
+
+    public static void menorLista(List<Integer> numeros){
+        int menor = numeros.stream()
+                    .mapToInt(x -> x)
+                    .min()
+                    .getAsInt();
+        System.out.println(menor);
+    }
+
+    public static void aMinusculas(List<String> palabras){
+        List<String> minusculas = palabras.stream()
+                    .map(x -> x.toLowerCase())
+                    .collect(Collectors.toList());
+        System.out.println(minusculas);
     }
 }
